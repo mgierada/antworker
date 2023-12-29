15ea31452542ada8d477ac799622675a99d9d9b9
Maciej Gierada <maciek.gierada@gmail.com>
2023-12-29 23:50:14 +0100

    feat: correctly processing subjects

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,6 @@ lazy_static! {
 }
 
 async fn connect() -> imap::error::Result<Option<String>> {
-    // let client = imap::ClientBuilder::new(&COMPANY_EMAIL_SERVER, &COMPANY_EMAIL_PORT).connect()?;
     let tls = native_tls::TlsConnector::builder().build().unwrap();
     let client = imap::connect(
         (COMPANY_EMAIL_SERVER.to_string(), *COMPANY_EMAIL_PORT),
@@ -34,8 +33,6 @@ async fn connect() -> imap::error::Result<Option<String>> {
     )
     .unwrap();
 
-    // the client we have here is unauthenticated.
-    // to do anything useful with the e-mails, we need to log in
     let mut imap_session = client
         .login(
             COMPANY_EMAIL.to_string(),
@@ -43,12 +40,9 @@ async fn connect() -> imap::error::Result<Option<String>> {
         )
         .map_err(|e| e.0)?;
 
-    // we want to fetch the first email in the INBOX mailbox
     imap_session.select("INBOX")?;
 
-    // fetch message number 1 in this mailbox, along with its RFC822 field.
-    // RFC 822 dictates the format of the body of e-mails
-    let messages = imap_session.fetch("2", "ENVELOPE")?;
+    let messages = imap_session.fetch("15", "ENVELOPE")?;
     // let messages = imap_session.fetch("1", "RFC822")?;
     let message = if let Some(m) = messages.iter().next() {
         m
@@ -60,13 +54,33 @@ async fn connect() -> imap::error::Result<Option<String>> {
         .envelope()
         .expect("message did not have an envelope!");
 
+    // let subject = if let Some(subject_bytes) = envelope.subject {
+    //     let decoded_subject = decode(subject_bytes, ParseMode::Robust).unwrap_or_else(|e| {
+    //         eprintln!("Failed to decode subject: {}", e);
+    //         Vec::new() // Return an empty Vec<u8> in case of decoding failure
+    //     });
+    //
+    //     String::from_utf8_lossy(&decoded_subject).to_string()
+    // } else {
+    //     // Handle the case when the subject is None (no subject in the envelope)
+    //     // You can choose to return an empty string or handle it differently based on your requirements.
+    //     String::new()
     let subject = if let Some(subject_bytes) = envelope.subject {
-        let decoded_subject = decode(subject_bytes, ParseMode::Robust).unwrap_or_else(|e| {
-            eprintln!("Failed to decode subject: {}", e);
-            Vec::new() // Return an empty Vec<u8> in case of decoding failure
-        });
+        let decoded_subject = decode(subject_bytes, ParseMode::Robust)
+            .map(|v| String::from_utf8_lossy(&v).to_string())
+            .unwrap_or_else(|e| {
+                eprintln!("Failed to decode subject: {}", e);
+                String::new() // Return an empty string in case of decoding failure
+            });
 
-        String::from_utf8_lossy(&decoded_subject).to_string()
+        // HACK: Who the hell knows why the resulted string has those utf related
+        // encoding characters.
+        let decoded_subject = decoded_subject
+            .replace("=?UTF-8?Q?", "") // Remove the prefix
+            .replace("?= ", "") // Remove the prefix
+            .replace("_", " "); // Replace underscores with spaces
+
+        decoded_subject
     } else {
         // Handle the case when the subject is None (no subject in the envelope)
         // You can choose to return an empty string or handle it differently based on your requirements.
