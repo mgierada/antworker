9f8747e5a2e8018b8eae45f40db07aa223a7eb47
Maciej Gierada <maciek.gierada@gmail.com>
2024-01-03 22:24:36 +0100

    feat: improve date parser

--- src/email_parser/parser.rs
+++ src/email_parser/parser.rs
@@ -7,9 +7,10 @@ use mailparse::{self, parse_mail};
 use native_tls::TlsStream;
 use quoted_printable::{decode, ParseMode};
 use std::{
+    collections::HashMap,
     fs::File,
     io::{Read, Write},
-    path::Path, collections::HashMap,
+    path::Path,
 };
 
 use crate::{
@@ -35,16 +36,18 @@ pub struct Credentials {
     pub password: String,
 }
 
-async fn connect(credentials: &Credentials) -> imap::error::Result<Session<TlsStream<std::net::TcpStream>>> {
+async fn connect(
+    credentials: &Credentials,
+) -> imap::error::Result<Session<TlsStream<std::net::TcpStream>>> {
     let tls = native_tls::TlsConnector::builder().build().unwrap();
     let client = imap::connect(
         (&*credentials.server, credentials.port), // Pass a reference to credentials.server
-        &credentials.server, // Pass a reference to credentials.server
+        &credentials.server,                      // Pass a reference to credentials.server
         &tls,
     )?;
     let imap_session = client
         .login(
-            &credentials.email, // Pass a reference to credentials.email
+            &credentials.email,    // Pass a reference to credentials.email
             &credentials.password, // Pass a reference to credentials.password
         )
         .map_err(|e| e.0)?;
@@ -61,10 +64,17 @@ fn fetch_emails<S: Read + Write>(
 }
 
 pub fn parse_date(date_str: &str) -> DateTime<Utc> {
+    // HACK: This is kind of a quacky solution.
+    // Attempt to parse the date string using RFC 2822 format
     let date = DateTime::parse_from_rfc2822(date_str)
         .map(|dt| dt.with_timezone(&Utc))
-        .unwrap_or_else(|e| {
-            eprintln!("Failed to parse date: {}", e);
+        .or_else(|_| {
+            // If parsing with RFC 2822 fails, try a custom format with full timezone name
+            let date_format = "%a %b %d %H:%M:%S %Z %Y";
+            DateTime::parse_from_str(date_str, date_format).map(|dt| dt.with_timezone(&Utc))
+        })
+        .unwrap_or_else(|_| {
+            // If all parsing attempts fail, fallback to current time in UTC
             Utc::now()
         });
     date
@@ -133,48 +143,6 @@ fn get_email_details(
     Ok(email_details)
 }
 
-pub fn save_attachments<S: Read + Write>(
-    messages: &ZeroCopy<Vec<Fetch>>,
-    imap_session: &mut Session<TlsStream<S>>,
-    // rules: &FilterRules,
-) -> imap::error::Result<()> {
-    for msg in messages.iter() {
-        let uid = msg.uid.unwrap();
-        let message_stream = imap_session.uid_fetch(uid.to_string(), "BODY[]").unwrap();
-        for fetch_result in &message_stream {
-            let body = fetch_result.body().unwrap();
-            // Parse the MIME content
-            let mail = parse_mail(body).unwrap();
-            // Iterate through MIME parts
-            for part in mail.subparts.iter() {
-                let content_type = &part.ctype;
-                match content_type.mimetype.as_str() {
-                    "application/pdf" => {
-                        let filename = content_type
-                            .params
-                            .get("name")
-                            .cloned()
-                            .unwrap_or_else(|| format!("attachment_{}_unnamed.pdf", uid));
-                        let binary_content = part
-                            .get_body_raw()
-                            .map_err(|e| eprintln!("Failed to get body raw: {}", e))
-                            .expect("Failed to get body raw");
-                        let mut file = File::create(filename.clone())
-                            .map_err(|e| eprintln!("Failed to create file: {}", e))
-                            .expect("Failed to create file");
-                        file.write_all(&binary_content)
-                            .map_err(|e| eprintln!("Failed to write to file: {}", e))
-                            .expect("Failed to write to file");
-                        println!("Attachment saved to file: {}", filename);
-                    }
-                    _ => {}
-                }
-            }
-        }
-    }
-    Ok(())
-}
-
 fn get_and_save_attachments<S: Read + Write>(
     email_details: &Vec<EmailDetails>,
     imap_session: &mut Session<TlsStream<S>>,
@@ -221,7 +189,9 @@ fn get_and_save_attachments<S: Read + Write>(
 }
 
 // Define a function to fetch emails for a given set of credentials
-async fn process_inbox(credentials: &Credentials) -> Result<Vec<EmailDetails>, Box<dyn std::error::Error>> {
+async fn process_inbox(
+    credentials: &Credentials,
+) -> Result<Vec<EmailDetails>, Box<dyn std::error::Error>> {
     let mut imap_session = connect(credentials).await?;
     let uid_set = "1:*";
     let messages = fetch_emails(&mut imap_session, &uid_set)?;
@@ -244,31 +214,29 @@ pub async fn process_all_inboxes(
         let email_details = process_inbox(credentials).await?;
         all_email_details.extend(email_details);
     }
-
     Ok(all_email_details)
 }
 
 pub async fn process_emails() -> Result<(), Box<dyn std::error::Error>> {
     let mut inboxes = HashMap::new();
-    inboxes.insert("company", Credentials {
-        server: COMPANY_EMAIL_SERVER.to_string(),
-        port: *COMPANY_EMAIL_PORT,
-        email: COMPANY_EMAIL.to_string(),
-        password: COMPANY_EMAIL_PASSWORD.to_string(),
-    });
-    inboxes.insert("private", Credentials {
-        server: COMPANY_EMAIL_SERVER.to_string(),
-        port: *COMPANY_EMAIL_PORT,
-        email: PRIVATE_EMAIL.to_string(),
-        password: PRIVATE_EMAIL_PASSWORD.to_string(),
-    });
-    
-    let credentials = Credentials {
-        server: COMPANY_EMAIL_SERVER.to_string(),
-        port: *COMPANY_EMAIL_PORT,
-        email: COMPANY_EMAIL.to_string(),
-        password: COMPANY_EMAIL_PASSWORD.to_string(),
-    };
+    inboxes.insert(
+        "company",
+        Credentials {
+            server: COMPANY_EMAIL_SERVER.to_string(),
+            port: *COMPANY_EMAIL_PORT,
+            email: COMPANY_EMAIL.to_string(),
+            password: COMPANY_EMAIL_PASSWORD.to_string(),
+        },
+    );
+    inboxes.insert(
+        "private",
+        Credentials {
+            server: COMPANY_EMAIL_SERVER.to_string(),
+            port: *COMPANY_EMAIL_PORT,
+            email: PRIVATE_EMAIL.to_string(),
+            password: PRIVATE_EMAIL_PASSWORD.to_string(),
+        },
+    );
 
     // Process emails for all inboxes
     if let Ok(email_details) = process_all_inboxes(inboxes).await {
@@ -277,14 +245,4 @@ pub async fn process_emails() -> Result<(), Box<dyn std::error::Error>> {
         eprintln!("Error processing emails for one or more inboxes");
     }
     Ok(())
-    
-    // let mut imap_session = connect(&credentials).await?;
-    // let uid_set = "1:*"; // get all emails
-    // let messages = fetch_emails(&mut imap_session, &uid_set)?;
-    // let rules = define_rules();
-    // let email_details = get_email_details(&messages, &rules)?;
-    // let save_location = setup_save_location()?;
-    // get_and_save_attachments(&email_details, &mut imap_session, &save_location);
-    // imap_session.logout()?;
-    // Ok(email_details)
 }
