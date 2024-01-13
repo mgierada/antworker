34946d147bb612fcbc86ddf6664072b38a0ead03
Maciej Gierada <maciek.gierada@gmail.com>
2024-01-13 14:21:28 +0100

    feat: abstact out attachment handling logic

---
+++ src/email_sender/attachment.rs
@@ -0,0 +1,26 @@
+use std::fs;
+
+use lettre::message::{header::ContentType, Attachment, SinglePart};
+
+use crate::io::files::get_saved_files;
+
+fn add_attachment(filepath: &String) -> SinglePart {
+    let filename = filepath
+        .split("/")
+        .collect::<Vec<&str>>()
+        .last()
+        .unwrap()
+        .to_string();
+    let filebody = fs::read(filepath).unwrap();
+    let content_type = ContentType::parse("application/pdf").unwrap();
+    Attachment::new(filename).body(filebody, content_type)
+}
+
+pub fn add_attachments() -> Vec<SinglePart> {
+    let save_location = get_saved_files();
+    let attachments = save_location
+        .iter()
+        .map(|filepath| add_attachment(filepath))
+        .collect();
+    attachments
+}
--- src/email_sender/sender.rs
+++ src/email_sender/sender.rs
@@ -1,13 +1,15 @@
 use indicatif::{ProgressBar, ProgressIterator, ProgressStyle};
 use lazy_static::lazy_static;
 use lettre::{
-    message::{header::ContentType, Attachment, MultiPart, SinglePart},
+    message::{header::ContentType, MultiPart, SinglePart},
     transport::smtp::authentication::Credentials,
     Message, SmtpTransport, Transport,
 };
-use std::{env::var, fs};
+use std::env::var;
 
 use crate::{io::files::get_saved_files, COMPANY_EMAIL, COMPANY_EMAIL_PASSWORD};
+
+use super::attachment::add_attachments;
 
 lazy_static! {
     pub static ref TARGET_EMAIL: String = var("TARGET_EMAIL").expect("TARGET_EMAIL must be set.");
@@ -31,28 +33,6 @@ fn format_email(email: &str) -> String {
     format!("{} <{}>", name, email)
 }
 
-fn add_attachment(filepath: &String) -> SinglePart {
-    let filename = filepath
-        .split("/")
-        .collect::<Vec<&str>>()
-        .last()
-        .unwrap()
-        .to_string();
-    // let file_to_send = format!("{}/{}", save_location, filename);
-    let filebody = fs::read(filepath).unwrap();
-    let content_type = ContentType::parse("application/pdf").unwrap();
-    Attachment::new(filename).body(filebody, content_type)
-}
-
-fn add_attachments() -> Vec<SinglePart> {
-    let save_location = get_saved_files();
-    let attachments = save_location
-        .iter()
-        .map(|filepath| add_attachment(filepath))
-        .collect();
-    attachments
-}
-
 pub fn send_emails(is_dry_run: bool) {
     let attachments = add_attachments();
     let n_attachments = attachments.len();
@@ -60,6 +40,7 @@ pub fn send_emails(is_dry_run: bool) {
     pb.set_style(
         ProgressStyle::with_template("{spinner:.green} [{bar:40.red}] ({pos}/{len})").unwrap(),
     );
+    
     if is_dry_run {
         let att = get_saved_files()
             .iter()
@@ -72,6 +53,7 @@ pub fn send_emails(is_dry_run: bool) {
         );
         return;
     }
+    
     attachments.iter().progress_with(pb).for_each(|attachment| {
         send_email(attachment.clone());
     });
@@ -94,12 +76,12 @@ pub fn send_email(attachment: SinglePart) -> () {
         .unwrap();
 
     let from = Credentials::new(COMPANY_EMAIL.to_owned(), COMPANY_EMAIL_PASSWORD.to_owned());
-    // Open a remote connection to gmail
+    
     let mailer = SmtpTransport::relay(SMTP_TARGET_SERVER.as_str())
         .unwrap()
         .credentials(from)
         .build();
-    // Send the email
+    
     match mailer.send(&email) {
         Ok(_) => println!("Email sent successfully!"),
         Err(e) => panic!("Could not send email: {e:?}"),
