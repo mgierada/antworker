b0d915122d456ae245f28a75d1dc9b66583056e2
Maciej Gierada <maciek.gierada@gmail.com>
2024-01-30 09:38:18 +0100

    feat: set schema for a new db

--- src/db/email.rs
+++ src/db/email.rs
@@ -5,31 +5,36 @@ use crate::datemath::date::get_current_year_month_str;
 use crate::db::connect::connect;
 use crate::email_parser::parser::EmailDetails;
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct EmailMonthly {
-    year_month: String,
-    emails: Emails,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Emails {
-    pub mailbox: String,
-    pub details: Vec<EmailDetails>,
-}
-
 #[derive(Debug, Deserialize)]
 struct Record {
     #[allow(dead_code)]
     id: Thing,
 }
 
-pub async fn store_emails(emails: Emails) -> surrealdb::Result<()> {
+#[derive(Debug, Serialize, Deserialize)]
+pub struct EmailMonthly {
+    pub year_month: String,
+    pub items: Vec<Mailbox>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Mailbox {
+    pub mailbox: Vec<MailboxDetails>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct MailboxDetails {
+    pub name: String,
+    pub emails: Vec<EmailDetails>,
+}
+
+pub async fn store_emails(items: Vec<Mailbox>) -> surrealdb::Result<()> {
     let db = connect().await?;
     let created: Vec<Record> = db
         .create("emails")
         .content(EmailMonthly {
             year_month: get_current_year_month_str(),
-            emails,
+            items,
         })
         .await?;
     dbg!(created);
