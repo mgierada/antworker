5ed4935a193e191ee6e05254863d56471f176331
Maciej Gierada <maciek.gierada@gmail.com>
2024-02-03 15:30:55 +0100

    feat: add CreateEmailMontly struct

--- src/crud/create_email.rs
+++ src/crud/create_email.rs
@@ -2,17 +2,16 @@ use crate::{
     datemath::date::get_current_year_month_str,
     db::{
         connect::connect,
-        email::{EmailMonthly, Emails, Record},
+        email::{CreateEmailMonthly, EmailMonthly, Emails, Record},
         enums::Tables,
     },
 };
 
-
 pub async fn store_emails(emails: Emails) -> surrealdb::Result<()> {
     let db = connect().await?;
     let _: Vec<Record> = db
-        .update(Tables::Emails.to_string())
-        .content(EmailMonthly {
+        .create(Tables::Emails.to_string())
+        .content(CreateEmailMonthly{
             year_month: get_current_year_month_str(),
             emails,
         })
--- src/db/email.rs
+++ src/db/email.rs
@@ -11,6 +11,13 @@ pub struct Record {
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct EmailMonthly {
+    pub id: Thing,
+    pub year_month: String,
+    pub emails: Emails,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct CreateEmailMonthly {
     pub year_month: String,
     pub emails: Emails,
 }
