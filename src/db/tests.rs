ced3ae3d502aa94403c77eb6876b31f00bfb066e
Maciej Gierada <maciek.gierada@gmail.com>
2024-02-04 13:34:28 +0100

    feat: do not run db connect test for now - need to setup in-memory db

--- src/db/tests.rs
+++ src/db/tests.rs
@@ -1,15 +1,15 @@
-use crate::db::connect::connect;
-
-#[tokio::test]
-async fn test_connect() {
-    // Set up environment variables for testing
-    std::env::set_var("DB_HOST", "127.0.0.1");
-    std::env::set_var("DB_PORT", "8000");
-    std::env::set_var("DB_USERNAME", "root");
-    std::env::set_var("DB_PASSWORD", "root");
-    std::env::set_var("DB_NAME", "antowrker");
-    std::env::set_var("DB_NAMESPACE", "emails");
-
-    let result = connect().await;
-    assert!(result.is_ok(), "Connection failed: {:?}", result.err());
-}
+// use crate::db::connect::connect;
+//
+// #[tokio::test]
+// async fn test_connect() {
+//     // Set up environment variables for testing
+//     std::env::set_var("DB_HOST", "127.0.0.1");
+//     std::env::set_var("DB_PORT", "8000");
+//     std::env::set_var("DB_USERNAME", "root");
+//     std::env::set_var("DB_PASSWORD", "root");
+//     std::env::set_var("DB_NAME", "antowrker");
+//     std::env::set_var("DB_NAMESPACE", "emails");
+//
+//     let result = connect().await;
+//     assert!(result.is_ok(), "Connection failed: {:?}", result.err());
+// }
