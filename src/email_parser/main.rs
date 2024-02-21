77520a6301382458c936a530e5ed1a94fa6d97ef
Maciej Gierada <maciek.gierada@gmail.com>
2024-02-21 23:52:46 +0100

    feat: pull latest_uid or use 1 if no latest_uid per mailbox while getting emails

--- src/email_parser/main.rs
+++ src/email_parser/main.rs
@@ -1,22 +1,26 @@
-use std::collections::HashMap;
-
+use crate::crud::get_mailbox::GetMailboxDbOps;
 use crate::{
-    factories::credentials::EmailAccountBuilder, COMPANY_EMAIL, COMPANY_EMAIL_PASSWORD,
-    COMPANY_EMAIL_PORT, COMPANY_EMAIL_SERVER, PRIVATE_EMAIL, PRIVATE_EMAIL_PASSWORD, S_EMAIL,
-    S_EMAIL_PASSWORD,
+    db::connect::DatabaseConnection, factories::credentials::EmailAccountBuilder, COMPANY_EMAIL,
+    COMPANY_EMAIL_PASSWORD, COMPANY_EMAIL_PORT, COMPANY_EMAIL_SERVER, PRIVATE_EMAIL,
+    PRIVATE_EMAIL_PASSWORD, S_EMAIL, S_EMAIL_PASSWORD,
 };
+use std::collections::HashMap;
 
 use super::inbox::process_all_inboxes;
 
 pub async fn process_emails() -> Result<(), Box<dyn std::error::Error>> {
+    let db_conn = DatabaseConnection;
     let mut inboxes = HashMap::new();
+    let latest_uid_company = db_conn.get_latest_uid_by_mailbox("company").await?;
+    dbg!(&latest_uid_company);
     let company_credentials = EmailAccountBuilder::new(
         &COMPANY_EMAIL_SERVER,
         *COMPANY_EMAIL_PORT,
         &COMPANY_EMAIL,
         &COMPANY_EMAIL_PASSWORD,
     )
-    .uid_set("1:*")
+    // .uid_set("1:*")
+    .uid_set(format!("{}:*", latest_uid_company.unwrap_or(1)).as_str())
     .build();
     let private_credentials = EmailAccountBuilder::new(
         &COMPANY_EMAIL_SERVER,
